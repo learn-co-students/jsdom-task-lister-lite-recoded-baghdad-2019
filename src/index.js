ddf8c0ef46cdea6f5b0114c8fece5d84964be300
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 12:30:15 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask);
+
+  let input = document.querySelector("#new-task-description");
+
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
